957e62795af98b2dbbead876698362259bff4a66
LukeOfficial <e87624273@gmail.com>
2024-03-21 11:47:42 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
-mdb = {}
+mdb = {
+  MAX_DATABASE_LIMIT = 20;
+}
 
 local dss = {}
 
 function mdb.registerdatastore(name)
   if dss[name] then
     error("ERROR[mdb]: Already exists")
+  else
+    dss[name]
   end
 end
